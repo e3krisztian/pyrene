7393777db15b5dc5d704506c99c373ebf4533dde
Fekete Krisztián <fekete.krisztyan@gmail.com>
2014-04-07 18:25:57 +0200

    moved RepoManager to separate file

--- reponet/main.py
+++ reponet/main.py
@@ -17,103 +17,9 @@ import tempfile
 import os
 import sys
 import shutil
-from ConfigParser import RawConfigParser
 from .util import write_file
 from .util import Directory
-from .repos import FileRepo, PipLocalRepo, HttpRepo, PyPIRepo
-
-
-REPOTYPE_FILE = 'file'
-REPOTYPE_HTTP = 'http'
-REPOTYPE_PYPI = 'pypi'
-REPOTYPE_PIPLOCAL = 'piplocal'
-
-
-class UnknownRepoError(NameError):
-    '''Repo is not defined at all'''
-
-
-class UndefinedRepoType(ValueError):
-    '''type was not defined for repo'''
-
-
-class UnknownRepoType(ValueError):
-    '''type was given, but it is unknown'''
-
-
-KEY_TYPE = 'type'
-KEY_DIRECTORY = 'directory'
-KEY_USERNAME = 'username'
-KEY_PASSWORD = 'password'
-KEY_DOWNLOAD_URL = 'download_url'
-KEY_UPLOAD_URL = 'upload_url'
-
-REPO_ATTRIBUTES = {
-    KEY_TYPE,
-    KEY_DIRECTORY,
-    KEY_DOWNLOAD_URL,
-    KEY_UPLOAD_URL,
-    KEY_USERNAME,
-    KEY_PASSWORD,
-}
-REPO_ATTRIBUTE_COMPLETIONS = tuple('{}='.format(a) for a in REPO_ATTRIBUTES)
-
-TYPE_TO_CLASS = {
-    REPOTYPE_FILE: FileRepo,
-    REPOTYPE_PIPLOCAL: PipLocalRepo,
-    REPOTYPE_HTTP: HttpRepo,
-    REPOTYPE_PYPI: PyPIRepo
-}
-
-
-class RepoManager(object):
-
-    def __init__(self, filename):
-        self._repo_store_filename = filename
-        self._config = RawConfigParser()
-        if os.path.exists(self._repo_store_filename):
-            self._config.read(self._repo_store_filename)
-
-    def _save(self):
-        with open(self._repo_store_filename, 'wt') as f:
-            self._config.write(f)
-
-    def get_repo(self, repo_name):
-        if not self._config.has_option(repo_name, KEY_TYPE):
-            if self._config.has_section(repo_name):
-                raise UndefinedRepoType(repo_name)
-            raise UnknownRepoError(repo_name)
-
-        attributes = self.get_attributes(repo_name)
-        repo_type = attributes[KEY_TYPE]
-
-        try:
-            return TYPE_TO_CLASS[repo_type](attributes)
-        except KeyError:
-            raise UnknownRepoType(repo_type)
-
-    def define(self, repo_name):
-        self._config.add_section(repo_name)
-        self._save()
-
-    def forget(self, repo_name):
-        self._config.remove_section(repo_name)
-        self._save()
-
-    def set(self, repo_name, key, value):
-        self._config.set(repo_name, key, value)
-        self._save()
-
-    @property
-    def repo_names(self):
-        return self._config.sections()
-
-    def get_attributes(self, repo_name):
-        attributes = {
-            option: self._config.get(repo_name, option)
-            for option in self._config.options(repo_name)
-        }
-        return attributes
+from .repomanager import RepoManager
 
 
 class BaseCmd(Cmd, object):
@@ -139,6 +45,12 @@ class BaseCmd(Cmd, object):
         print('Bye!')
         return True
     do_bye = do_EOF
+
+
+REPO_ATTRIBUTE_COMPLETIONS = tuple(
+    '{}='.format(a)
+    for a in RepoManager.REPO_ATTRIBUTES
+)
 
 
 class RepoNetCmd(BaseCmd):
@@ -253,7 +165,7 @@ class RepoNetCmd(BaseCmd):
             )
         elif '=' in words[-1]:
             if words[-1].startswith('type='):
-                completions = tuple(TYPE_TO_CLASS)
+                completions = tuple(RepoManager.REPO_TYPES)
         else:
             completions = REPO_ATTRIBUTE_COMPLETIONS
         return sorted(c for c in completions if c.startswith(text))
